088327b483409c335b3c1813060801af97fdfdc0
Jay Xu <jay.xu.krfantasy@gmail.com>
2018-12-14 11:26:28 +0800

    added _mm_extract_epi32 _mm_extract_ps _mm_isinf_ps

---
+++ SSE2NEONUtils.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <math.h>
+
+#include "SSE2NEON.h"
+
+static inline __m128  _mm_isinf_ps(__m128 x)
+{
+	__m128 SIGN_MASK = _mm_set1_ps(-0.0);
+	__m128 INF = _mm_set1_ps(INFINITY);
+
+	return _mm_cmpeq_ps(INF, _mm_andnot_ps(SIGN_MASK, x));
+}
